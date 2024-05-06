ac68d4ea8f1dfc7c0d77ccf711dc55942e25e126
Nguyen Kim Huy <kimhuy011199@gmail.com>
2024-05-06 14:25:53 +0700

    Init shadcn ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
+import { Button } from './components/ui/button';
+
 const App = () => {
   return (
     <div>
       <h1 className="text-6xl text-green-500 font-bold underline">
         Hello world!
       </h1>
+      <Button>Click me</Button>
     </div>
   );
 };
